10701a22f272fd4faf9704f8d6e67b1597801555
Eric <2006218@newton.pe>
2021-03-29 15:41:29 -0500

    GUI / Controller - 29/03/2021

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,26 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
 
 public class Controller {
+
+    @FXML private TextField itemTxtbox;
+    @FXML private TextField servingsTxtbox;
+    @FXML private TextField qtyTxtbox;
+    @FXML private ChoiceBox typeChoicebox;
+    @FXML private DatePicker xpirationDatepicker;
+    @FXML private ListView productList;
+
+    ArrayList <Item>
+
+
     public void addToListButton(ActionEvent actionEvent) {
     }
 
